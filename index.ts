470bd4d9f847ada67eaf70270cc6317a9c91d1c8
vicWithAi <victor.maina@boxraft.com>
2024-01-16 05:45:31 +0000

    feat: "chromium added"

--- index.ts
+++ index.ts
@@ -1,29 +1,30 @@
-const puppeteer = require('puppeteer');
-const { Browser } = puppeteer;
+import puppeteer from 'puppeteer-core';
+import chromium from 'chrome-aws-lambda';
 
-const url ='https://books.toscrape.com';
-//https://mookh.com/
-
+const url = 'https://books.toscrape.com';
 
 const main = async () => {
-    const browser = await puppeteer.launch({headless:false,args: ["--no-sandbox", "--disable-setuid-sandbox"]})
-    
-    const page = await browser.newPage()
-    await page.goto(url)
+    const browser = await puppeteer.launch({ 
+        headless: true, 
+        executablePath: await chromium.executablePath, 
+      });
 
-    const eventData = await page.evaluate((url:string) => {
-        const eventdeets = Array.from(document.querySelectorAll('.product_pod')); //.sc-dkmKpi.lfQrzt class for events
-        const data = eventdeets.map((book:any)=>({
-            title:book.querySelector('h3 a').getAttribute('title'),
-            price: book.querySelector('price_color').innerText,
-            imgSrc: url + book.querySelector('img').getAttribute('src')
-        }))
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const eventData = await page.evaluate((url: string) => {
+        const eventdeets = Array.from(document.querySelectorAll('.product_pod'));
+        const data = eventdeets.map((book: any) => ({
+            title: book.querySelector('h3 a').getAttribute('title'),
+            price: (book.querySelector('price_color') as HTMLElement).innerText,
+            imgSrc: url + book.querySelector('img').getAttribute('src'),
+        }));
         return data;
-    }, url)
+    }, url);
 
     console.log(eventData);
 
     await browser.close();
-}
+};
 
 main();
